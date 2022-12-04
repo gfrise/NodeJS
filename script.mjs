9bdf574a1878cab4ae91f941d6e033b69205ab43
gfrise <104461249+gfrise@users.noreply.github.com>
2022-12-04 15:33:58 +0100

    es11

--- script.mjs
+++
@@ -1,9 +0,0 @@
-import * as fs from 'node:fs'
-
-const data = "We will rock you!"
-
-fs.writeFile("file.txt", data, {encoding: 'utf8'}, function(error) {
-    if (error) {
-        console.log(error)
-    }
-})
