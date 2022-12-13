7aafff7b4763965813a49275ebfb1efd168fe2c2
gfrise <104461249+gfrise@users.noreply.github.com>
2022-12-13 18:51:24 +0100

    bo

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,7 @@ import "dotenv/config"
 
 const app = express()
 
-const port = process.env.PORT 
+const port = process.env.PORT
 
 app.get("/", (request, response) => {
     response.json('Hola a todos')
@@ -14,3 +14,10 @@ app.listen(port, () => {
 })
 
 export default app
+
+// Create a database and Prisma model
+// In this exercise you'll build on what you created 
+// in 'Exercise: Create your own HTTP server'.
+// Create a database for your application and 
+// configure Prisma to use it. Then create a Prisma model 
+// and a migration. Choose your own theme for the model.
